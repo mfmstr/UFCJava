ca70cbb9cc56f99ef098ee3b34213e7c3c0d5fb1
mfmstr <106274061+mfmstr@users.noreply.github.com>
2022-09-14 17:15:42 +0400

    Add files via upload

---
+++ UFCL33/UFCL33/src/ufc/l33/main/Figure.java
@@ -0,0 +1,16 @@
+package ufc.l33.main;
+
+public abstract class Figure implements Comparable<Figure>{
+	
+	public abstract double getLength();
+
+	public int compareTo(Figure another) {
+		return Double.compare(this.getLength(), another.getLength());
+	}
+	
+	public boolean equals(Object obj) {
+		Circle a = (Circle) obj;
+		return this.getLength() == a.getLength();
+	}
+
+}
