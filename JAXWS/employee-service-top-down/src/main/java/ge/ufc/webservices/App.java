acdaf3091d7c277a95593f63329f276cbd7925c2
mfmstr <106274061+mfmstr@users.noreply.github.com>
2022-10-13 22:14:38 +0400

    Add files via upload

---
+++ JAXWS/employee-service-top-down/src/main/java/ge/ufc/webservices/App.java
@@ -0,0 +1,13 @@
+package ge.ufc.webservices;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
